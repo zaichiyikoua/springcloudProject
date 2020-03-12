018511e73c551c20620a113db47b879d6852bfb6
zaichiyikou <913475430@qq.com>
2020-03-12 16:55:29 +0800

    2020-3-12 cloudEurekaServer构建完成

---
+++ cloud-eureka-server7001/src/main/java/com/zaichiyikou/starter/EurekaServer7001.java
@@ -0,0 +1,14 @@
+package com.zaichiyikou.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//开启注解
+@EnableEurekaServer
+public class EurekaServer7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7001.class, args);
+    }
+}
