825e6d7849b77cd4662ce3ceffd8c3b10aa4d195
zaichiyikou <913475430@qq.com>
2020-03-12 17:10:02 +0800

    2020-3-12 cloud生产者服务注册eureka完成

--- cloud-provider-payment8001/src/main/java/com/zaichiyikou/starter/CloudProviderPayment8001.java
+++ cloud-provider-payment8001/src/main/java/com/zaichiyikou/starter/CloudProviderPayment8001.java
@@ -2,9 +2,11 @@ package com.zaichiyikou.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@MapperScan(basePackages = "com.zaichiyikou.starter.dao")
+@EnableEurekaClient
 public class CloudProviderPayment8001 {
     public static void main(String[] args) {
         SpringApplication.run(CloudProviderPayment8001.class, args);
